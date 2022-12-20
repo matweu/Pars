6ebd59e15280c88f67cc6017a2a9f7a6e3456b0c
Matwey <zhukall@mail.ru>
2022-12-20 03:20:54 +0300

    Parsing Set 750 (2)

--- parsing.cpp
+++ parsing.cpp
@@ -3,61 +3,12 @@
 
 #include <iostream>
 #include <math.h>
-#include <conio.h>
-#include <string.h>
-#include <windows.h>
 #include <fstream>
-#include <ctime>
 #include <Windows.h>
 #include <string>
 #pragma hdrstop
 using namespace std;
 
-/*int main()
-{
-        struct student
-        {
-            char fam_group[30];
-            char name_group[30];
-        };
-
-        SetConsoleCP(1251);
-        SetConsoleOutputCP(1251);
-        setlocale(LC_ALL, "Russian");
-        int k = 0, n = 0;
-        ifstream in_file;
-        ofstream out_file;
-        struct student b[1000];
-
-        in_file.open("cash.txt");
-        if (in_file.is_open())
-        {
-            while (!in_file.eof())
-            {
-                in_file >> b[k].name_group;
-                in_file >> b[k].fam_group;
-                k++;
-            }
-            // in_file.close();
-        }
-            else
-        {
-            {
-                cout << "|||||||||||||||||||||||||||||||||||" << endl;
-                cout << "No file: cash.txt" << "\n";
-                cout << "|||||||||||||||||||||||||||||||||||" << endl;
-            }
-            cout << endl;
-        }
-        for (int i = 0; i < n; i++)
-        {
-            cout << b[k].fam_group;
-        }
-        cout << "\n";
-
-        return(0);
-    }
-    */
 struct student
 {
     char name[40];
